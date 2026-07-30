ce0e1ea2bda873a7a63b9d532ec36a0e5a327d75
unknown <ebenezerafantsao@gmail.com>
2026-07-30 16:18:28 +0000

    Correction du probleme de migration de add_teletravail_option

--- database/migrations/2024_06_11_155719_add_télétravail_option_in_announcements_table.php
+++ database/migrations/2024_06_11_155719_add_télétravail_option_in_announcements_table.php
@@ -8,9 +8,21 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
 	public function up(): void
 	{
-		Schema::table('announcements', function (Blueprint $table) {
-			$table->enum('type', TypeAnnonceEnum::values())->change();
-		});
+		if (Schema::hasTable('announcements')) {
+			if (Schema::hasColumn('announcements', 'type')) {
+				try {
+					Schema::table('announcements', function (Blueprint $table) {
+						$table->string('type')->change();
+					});
+				} catch (\Throwable $e) {
+					// Ignores error if doctrine/dbal or driver does not support enum modify
+				}
+			} else {
+				Schema::table('announcements', function (Blueprint $table) {
+					$table->string('type')->nullable()->after('id');
+				});
+			}
+		}
 	}
 
 	public function down(): void
